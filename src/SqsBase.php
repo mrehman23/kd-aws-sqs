dbc4e853235b7bed57e117d6eb46ad0c81dfbf0b
MatiurRehman <38315196+mrehman23@users.noreply.github.com>
2023-08-14 22:35:05 +0500

    Created SqsBase.php

---
+++ src/SqsBase.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace SqsSimple;
+
+use Aws\Credentials\Credentials;
+use Aws\Sdk;
+use Aws\Sqs\SqsClient;
+
+class SqsBase
+{
+    public $sqsClient = null;
+
+    /**
+     * SqsBase constructor.
+     *
+     * @param array $awsConfig
+     */
+    public function __construct(array $awsConfig)
+    {
+        $sharedConfig = [
+            'region'  => $awsConfig['AWS_REGION'],
+            'version' => $awsConfig['API_VERSION'],
+        ];
+
+        if (isset($awsConfig['AWS_KEY'], $awsConfig['AWS_SECRET_KEY'])) {
+            $sharedConfig['credentials'] = new Credentials($awsConfig['AWS_KEY'], $awsConfig['AWS_SECRET_KEY']);
+        }
+
+        // Create an Amazon SQS client using the shared configuration data.
+        $sdk = new Sdk($sharedConfig);
+
+        $this->sqsClient = $sdk->createSqs();
+    }
+
+    /**
+     * Set client.
+     *
+     * @param SqsClient $sqsClient
+     */
+    public function setClient(SqsClient $sqsClient)
+    {
+        $this->sqsClient = $sqsClient;
+    }
+
+    /**
+     * Set params.
+     *
+     * @param array $params
+     */
+    public function setParams(array $params)
+    {
+        foreach ($params as $param => $value) {
+            $this->{$param} = $value;
+        }
+    }
+}
